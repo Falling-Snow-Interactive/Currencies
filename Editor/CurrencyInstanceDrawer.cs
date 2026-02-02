1486befef012f96c3bad7593de454b168d4a7d6a
Kira Dinan <kiradinan.dev@gmail.com>
2026-02-01 22:19:10 -0400

    Update to USS

--- Editor/CurrencyInstanceDrawer.cs
+++ Editor/CurrencyInstanceDrawer.cs
@@ -10,63 +10,64 @@ namespace Fsi.Currencies
     {
         private const string USS = "Packages/com.fallingsnowinteractive.currencies/Editor/CurrentInstanceDrawer.uss";
         
-        private static bool IsInListView(VisualElement element)
-        {
-            for (VisualElement current = element; current != null; current = current.parent)
-            {
-                if (current is MultiColumnListView || current is ListView)
-                {
-                    return true;
-                }
-
-                if (current.ClassListContains("unity-collection-view__item"))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
+        // private static bool IsInListView(VisualElement element)
+        // {
+        //     for (VisualElement current = element; current != null; current = current.parent)
+        //     {
+        //         if (current is MultiColumnListView || current is ListView)
+        //         {
+        //             return true;
+        //         }
+        //
+        //         if (current.ClassListContains("unity-collection-view__item"))
+        //         {
+        //             return true;
+        //         }
+        //     }
+        //
+        //     return false;
+        // }
 
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
-            VisualElement root = new();
-            
-            StyleSheet stylesheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(USS);
-            if (stylesheet)
-            {
-                root.styleSheets.Add(stylesheet);
-            }
-            
-            root.AddToClassList("currency");
-            FsiUiEditorUtility.AddUss(root);
-            
-            SerializedProperty currencyProp = property.FindPropertyRelative("currency");
-            SerializedProperty amountProp = property.FindPropertyRelative("amount");
-            
-            Label label = new(property.displayName);
-            label.AddToClassList("currency_label");
-            label.RegisterCallback<AttachToPanelEvent>(_ =>
-                                                       {
-                                                           label.style.display = IsInListView(root) 
-                                                                                     ? DisplayStyle.None 
-                                                                                     : DisplayStyle.Flex;
-                                                       });
-            root.Add(label);
-            
-            VisualElement data = new();
-            data.AddToClassList("currency_value");
-            root.Add(data);
-            
-            PropertyField amountField = new(amountProp, string.Empty);
-            amountField.AddToClassList("currency_amount");
-            data.Add(amountField);
-
-            PropertyField currencyField = new(currencyProp);
-            currencyField.AddToClassList("currency_data");
-            data.Add(currencyField);
-            
-            return root;
+            return new CurrencyInstanceElement(property.displayName, property);
+            // VisualElement root = new();
+            //
+            // StyleSheet stylesheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(USS);
+            // if (stylesheet)
+            // {
+            //     root.styleSheets.Add(stylesheet);
+            // }
+            //
+            // root.AddToClassList("currency");
+            // FsiUiEditorUtility.AddUss(root);
+            //
+            // SerializedProperty currencyProp = property.FindPropertyRelative("currency");
+            // SerializedProperty amountProp = property.FindPropertyRelative("amount");
+            //
+            // Label label = new(property.displayName);
+            // label.AddToClassList("currency_label");
+            // label.RegisterCallback<AttachToPanelEvent>(_ =>
+            //                                            {
+            //                                                label.style.display = IsInListView(root) 
+            //                                                                          ? DisplayStyle.None 
+            //                                                                          : DisplayStyle.Flex;
+            //                                            });
+            // root.Add(label);
+            //
+            // VisualElement data = new();
+            // data.AddToClassList("currency_value");
+            // root.Add(data);
+            //
+            // PropertyField amountField = new(amountProp, string.Empty);
+            // amountField.AddToClassList("currency_amount");
+            // data.Add(amountField);
+            //
+            // PropertyField currencyField = new(currencyProp);
+            // currencyField.AddToClassList("currency_data");
+            // data.Add(currencyField);
+            //
+            // return root;
         }
     }
 }
---
+++ Editor/CurrencyInstanceElement.cs
@@ -0,0 +1,83 @@
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Fsi.Currencies
+{
+    public class CurrencyInstanceElement : VisualElement
+    {
+        private const string USS = "Packages/com.fallingsnowinteractive.currencies/Editor/CurrentInstanceElement.uss";
+        
+        #region USS Classes
+
+        private const string RootClass = "currency_instance_element__root";
+        private const string LabelClass = "currency_instance_element__label";
+        private const string FieldClass = "currency_instance_element__property";
+        private const string FieldAmountClass = "currency_instance_element__amount";
+        private const string FieldCurrencyClass = "currency_instance_element__data";
+        private const string FieldSpacerClass = "field_space";
+
+        #endregion
+        
+        private readonly Label rootLabel;
+        
+        public CurrencyInstanceElement(string label, SerializedProperty property)
+        {
+            StyleSheet styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(USS);
+            if (styleSheet)
+            {
+                styleSheets.Add(styleSheet);
+            }
+            
+            VisualElement root = new();
+            root.AddToClassList(RootClass);
+            Add(root);
+
+            rootLabel = new Label(label);
+            rootLabel.AddToClassList(LabelClass);
+            root.Add(rootLabel);
+
+            VisualElement fieldGroup = new();
+            fieldGroup.AddToClassList(FieldClass);
+            root.Add(fieldGroup);
+
+            SerializedProperty amountProp = property.FindPropertyRelative("amount");
+            PropertyField minField = new(amountProp) { label = "" };
+            minField.AddToClassList(FieldAmountClass);
+            fieldGroup.Add(minField);
+
+            // VisualElement fieldSpacer = new();
+            // fieldSpacer.AddToClassList(FieldSpacerClass);
+            // fieldGroup.Add(fieldSpacer);
+
+            SerializedProperty currencyProp = property.FindPropertyRelative("currency");
+            PropertyField currencyField = new(currencyProp) { label = "" };
+            currencyField.AddToClassList(FieldCurrencyClass);
+            fieldGroup.Add(currencyField);
+
+            // When used inside a MultiColumnListView cell, hide the top label.
+            RegisterCallback<AttachToPanelEvent>(_ => UpdateRootLabelVisibility());
+        }
+        
+        private void UpdateRootLabelVisibility()
+        {
+            bool hide = IsInMultiColumnListView();
+            rootLabel.style.display = hide ? DisplayStyle.None : DisplayStyle.Flex;
+        }
+
+        private bool IsInMultiColumnListView()
+        {
+            for (VisualElement p = this; p != null; p = p.parent)
+            {
+                if (p is MultiColumnListView)
+                    return true;
+
+                // Fallback: Unity's internal root class name for the control.
+                if (p.ClassListContains("unity-multi-column-list-view"))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
